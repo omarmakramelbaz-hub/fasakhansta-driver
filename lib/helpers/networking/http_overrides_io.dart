2db181dd8385b74b137bafb8d7ca71b792cb6863
omarmakramelbaz-hub <omarmakramelbaz@gmail.com>
2026-08-30 18:50:11 +0300

    Preserve mobile HTTP overrides

---
+++ lib/helpers/networking/http_overrides_io.dart
@@ -0,0 +1,13 @@
+import 'dart:io';
+
+void configureHttpOverrides() {
+  HttpOverrides.global = MyHttpOverrides();
+}
+
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback = (X509Certificate cert, String host, int port) => true;
+  }
+}
