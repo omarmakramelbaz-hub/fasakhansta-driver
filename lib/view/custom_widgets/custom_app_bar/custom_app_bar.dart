c25e1258d7f03c7ba27a5c016f45aa9638e5ad24
omarmakramelbaz-hub <omarmakramelbaz@gmail.com>
2026-09-01 23:27:05 +0300

    Modernize shared app bars

--- lib/view/custom_widgets/custom_app_bar/custom_app_bar.dart
+++ lib/view/custom_widgets/custom_app_bar/custom_app_bar.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 import '../../../helpers/theme/app_colors.dart';
-import '../../../helpers/utils/general_const.dart';
 
 class CustomAppBar extends PreferredSize {
   final double height;
@@ -19,11 +18,12 @@ class CustomAppBar extends PreferredSize {
   final BuildContext context;
   final BorderRadiusGeometry? borderRadius;
   final double? leadingPadding;
+
   CustomAppBar(
     this.context, {
     super.key,
-    this.height = 110,
-    this.radius = genRadius,
+    this.height = 92,
+    this.radius = 24,
     this.elevation = 0,
     this.leading,
     this.actions,
@@ -37,66 +37,99 @@ class CustomAppBar extends PreferredSize {
     this.borderRadius,
     this.leadingPadding,
   }) : super(
-         preferredSize: Size.fromHeight(height),
-         child: AppBar(
-           elevation: elevation,
-           backgroundColor: appBarColor ?? AppColor.whiteColor(context),
-           toolbarHeight: height,
-           automaticallyImplyLeading: automaticallyImplyLeading,
-           shadowColor: shadowColor,
-           centerTitle: centerTitle,
-           title: Padding(
-             padding: const EdgeInsets.only(top: 22),
-             child:
-                 title ??
-                 SizedBox(
-                   height: height,
-                   child: const Column(mainAxisAlignment: MainAxisAlignment.end, children: []),
-                 ),
-           ),
-           leading: Padding(
-             padding: EdgeInsets.only(bottom: leadingPadding ?? 0, top: 10),
-             child: automaticallyImplyLeading && Navigator.canPop(context) && leading == null
-                 ? Center(
-                     child: IconButton(
-                       onPressed: Navigator.canPop(context)
-                           ? () {
-                               Navigator.pop(context);
-                             }
-                           : null,
-                       icon: Icon(Icons.arrow_back_ios_rounded, color: AppColor.whiteColor(context)),
-                     ),
-                   )
-                 : leading,
-           ),
-           actions: actions,
-           leadingWidth: leadingWidth,
-           // flexibleSpace: ClipRRect(
-           //   borderRadius: borderRadius ??
-           //       BorderRadius.vertical(
-           //         bottom: Radius.circular(radius),
-           //       ),
-           //   child: Container(
-           //     decoration: BoxDecoration(
-           //       borderRadius: borderRadius ??
-           //           BorderRadius.vertical(
-           //             bottom: Radius.circular(radius),
-           //           ),
-           //       gradient: LinearGradient(
-           //         begin: Alignment.topCenter,
-           //         end: Alignment.bottomCenter,
-           //         colors: <Color>[
-           //           AppColor.appBarColor(context),
-           //           AppColor.appBarColor2(context)
-           //         ],
-           //       ),
-           //     ),
-           //   ),
-           // ),
-           shape: RoundedRectangleBorder(
-             borderRadius: borderRadius ?? BorderRadius.vertical(bottom: Radius.circular(radius)),
-           ),
-           bottom: bottom,
-         ),
-       );
+          preferredSize: Size.fromHeight(height),
+          child: AppBar(
+            elevation: elevation,
+            scrolledUnderElevation: 0,
+            backgroundColor: appBarColor ?? AppColor.whiteColor(context),
+            surfaceTintColor: Colors.transparent,
+            toolbarHeight: height,
+            automaticallyImplyLeading: false,
+            shadowColor: shadowColor ?? const Color(0xff082A4D).withOpacity(.08),
+            centerTitle: centerTitle ?? false,
+            titleSpacing: 14,
+            title: Padding(
+              padding: const EdgeInsets.only(top: 8),
+              child: title ?? const SizedBox(),
+            ),
+            leading: leading ??
+                (automaticallyImplyLeading && Navigator.canPop(context)
+                    ? Padding(
+                        padding: EdgeInsets.only(
+                          top: 10,
+                          bottom: leadingPadding == null ? 10 : (leadingPadding! > 24 ? 18 : leadingPadding!),
+                          left: 6,
+                          right: 6,
+                        ),
+                        child: _PremiumBackButton(context: context),
+                      )
+                    : null),
+            actions: actions,
+            leadingWidth: leadingWidth ?? 62,
+            flexibleSpace: IgnorePointer(
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: borderRadius ?? BorderRadius.vertical(bottom: Radius.circular(radius)),
+                  gradient: appBarColor == null
+                      ? const LinearGradient(
+                          begin: Alignment.topLeft,
+                          end: Alignment.bottomRight,
+                          colors: [Color(0xfffffbf7), Color(0xffffffff)],
+                        )
+                      : null,
+                  boxShadow: [
+                    BoxShadow(
+                      color: const Color(0xff082A4D).withOpacity(.045),
+                      blurRadius: 18,
+                      offset: const Offset(0, 7),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            shape: RoundedRectangleBorder(
+              borderRadius: borderRadius ?? BorderRadius.vertical(bottom: Radius.circular(radius)),
+            ),
+            bottom: bottom,
+          ),
+        );
+}
+
+class _PremiumBackButton extends StatelessWidget {
+  const _PremiumBackButton({required this.context});
+
+  final BuildContext context;
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.white,
+      borderRadius: BorderRadius.circular(14),
+      child: InkWell(
+        borderRadius: BorderRadius.circular(14),
+        onTap: () => Navigator.maybePop(this.context),
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(14),
+            border: Border.all(color: const Color(0xffECEEF1)),
+            boxShadow: [
+              BoxShadow(
+                color: const Color(0xff082A4D).withOpacity(.07),
+                blurRadius: 12,
+                offset: const Offset(0, 5),
+              ),
+            ],
+          ),
+          alignment: Alignment.center,
+          child: Icon(
+            Directionality.of(context) == TextDirection.rtl
+                ? Icons.arrow_forward_rounded
+                : Icons.arrow_back_rounded,
+            color: const Color(0xffFD7201),
+            size: 24,
+          ),
+        ),
+      ),
+    );
+  }
 }
