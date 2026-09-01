a2a351e09ddc0bd4330db8eeea737a9953559aae
omarmakramelbaz-hub <omarmakramelbaz@gmail.com>
2026-09-01 23:26:20 +0300

    Apply premium delivery app color system

--- lib/helpers/theme/app_colors.dart
+++ lib/helpers/theme/app_colors.dart
@@ -8,43 +8,43 @@ class AppColor {
   }
 
   static Color main2AppColor(BuildContext context, {bool listen = true}) {
-    return AppTheme.getByTheme(context, light: const Color(0xffF47221), dark: const Color(0xffF47221), listen: listen);
+    return AppTheme.getByTheme(context, light: const Color(0xffFF8A08), dark: const Color(0xffFF8A08), listen: listen);
   }
 
   static Color lightMainAppColor(BuildContext context, {bool listen = true}) {
-    return AppTheme.getByTheme(context, light: const Color(0xffFFC18E), dark: const Color(0xffFFC18E), listen: listen);
+    return AppTheme.getByTheme(context, light: const Color(0xffFFF1E5), dark: const Color(0xffFFF1E5), listen: listen);
   }
 
   static Color lightDarkColor(BuildContext context, {bool listen = true}) {
-    return AppTheme.getByTheme(context, light: const Color(0xff323943), dark: const Color(0xff323943), listen: listen);
+    return AppTheme.getByTheme(context, light: const Color(0xff082A4D), dark: const Color(0xff082A4D), listen: listen);
   }
 
   static Color secondAppColor(BuildContext context, {bool listen = true}) {
-    return AppTheme.getByTheme(context, light: const Color(0xff000000), dark: const Color(0xff000000), listen: listen);
+    return AppTheme.getByTheme(context, light: const Color(0xff082A4D), dark: const Color(0xff082A4D), listen: listen);
   }
 
   static Color scaffoldColor(BuildContext context, {bool listen = true}) {
-    return AppTheme.getByTheme(context, light: const Color(0xffFFFFFF), dark: const Color(0xffFFFFFF), listen: listen);
+    return AppTheme.getByTheme(context, light: const Color(0xffF8F9FB), dark: const Color(0xffF8F9FB), listen: listen);
   }
 
   static Color hintColor(BuildContext context, {bool listen = true}) {
-    return AppTheme.getByTheme(context, light: const Color(0xffAEAEAE), dark: const Color(0xffAEAEAE), listen: listen);
+    return AppTheme.getByTheme(context, light: const Color(0xffAEB3BA), dark: const Color(0xffAEB3BA), listen: listen);
   }
 
   static Color darkTextColor(BuildContext context, {bool listen = true}) {
-    return AppTheme.getByTheme(context, light: const Color(0xff000000), dark: const Color(0xff000000), listen: listen);
+    return AppTheme.getByTheme(context, light: const Color(0xff082A4D), dark: const Color(0xff082A4D), listen: listen);
   }
 
   static Color lightTextColor(BuildContext context, {bool listen = true}) {
-    return AppTheme.getByTheme(context, light: const Color(0xff727272), dark: const Color(0xff727272), listen: listen);
+    return AppTheme.getByTheme(context, light: const Color(0xff7D8490), dark: const Color(0xff7D8490), listen: listen);
   }
 
   static Color greyColor(BuildContext context, {bool listen = true}) {
-    return AppTheme.getByTheme(context, light: const Color(0xff818181), dark: const Color(0xff818181), listen: listen);
+    return AppTheme.getByTheme(context, light: const Color(0xff9AA0AA), dark: const Color(0xff9AA0AA), listen: listen);
   }
 
   static Color darkGreyColor(BuildContext context, {bool listen = true}) {
-    return AppTheme.getByTheme(context, light: const Color(0xff727272), dark: const Color(0xff727272), listen: listen);
+    return AppTheme.getByTheme(context, light: const Color(0xff6D7480), dark: const Color(0xff6D7480), listen: listen);
   }
 
   static Color whiteColor(BuildContext context, {bool listen = true}) {
@@ -52,7 +52,7 @@ class AppColor {
   }
 
   static Color offWhiteColor(BuildContext context, {bool listen = true}) {
-    return AppTheme.getByTheme(context, light: const Color(0xffF4F5F7), dark: const Color(0xffF4F5F7), listen: listen);
+    return AppTheme.getByTheme(context, light: const Color(0xffF7F8FA), dark: const Color(0xffF7F8FA), listen: listen);
   }
 
   static Color textFormFillColor(BuildContext context, {bool listen = true}) {
@@ -60,19 +60,19 @@ class AppColor {
   }
 
   static Color otpFillColor(BuildContext context, {bool listen = true}) {
-    return AppTheme.getByTheme(context, light: const Color(0xFFffffff), dark: const Color(0xFFffffff), listen: listen);
+    return AppTheme.getByTheme(context, light: const Color(0xFFFFFFFF), dark: const Color(0xFFFFFFFF), listen: listen);
   }
 
   static Color textFormBorderColor(BuildContext context, {bool listen = true}) {
-    return AppTheme.getByTheme(context, light: const Color(0xffDBDBDB), dark: const Color(0xffDBDBDB), listen: listen);
+    return AppTheme.getByTheme(context, light: const Color(0xffDADDE2), dark: const Color(0xffDADDE2), listen: listen);
   }
 
   static Color appBarColor(BuildContext context, {bool listen = true}) {
-    return AppTheme.getByTheme(context, light: const Color(0xffFD7201), dark: const Color(0xffFD7201), listen: listen);
+    return AppTheme.getByTheme(context, light: const Color(0xffFF8A08), dark: const Color(0xffFF8A08), listen: listen);
   }
 
   static Color appBarColor2(BuildContext context, {bool listen = true}) {
-    return AppTheme.getByTheme(context, light: const Color(0xffFF9D4D), dark: const Color(0xffFF9D4D), listen: listen);
+    return AppTheme.getByTheme(context, light: const Color(0xffFF6500), dark: const Color(0xffFF6500), listen: listen);
   }
 
   static Color buttonTextColor(BuildContext context, {bool listen = true}) {
@@ -80,11 +80,11 @@ class AppColor {
   }
 
   static Color blackColor(BuildContext context, {bool listen = true}) {
-    return AppTheme.getByTheme(context, light: const Color(0xff000000), dark: const Color(0xff000000), listen: listen);
+    return AppTheme.getByTheme(context, light: const Color(0xff111827), dark: const Color(0xff111827), listen: listen);
   }
 
   static Color textFormColor(BuildContext context, {bool listen = true}) {
-    return AppTheme.getByTheme(context, light: const Color(0xff000000), dark: const Color(0xff000000), listen: listen);
+    return AppTheme.getByTheme(context, light: const Color(0xff082A4D), dark: const Color(0xff082A4D), listen: listen);
   }
 
   static Color popupColor(BuildContext context, {bool listen = true}) {
@@ -92,15 +92,15 @@ class AppColor {
   }
 
   static Color appBarTextColor(BuildContext context, {bool listen = true}) {
-    return AppTheme.getByTheme(context, light: const Color(0xffFFFFFF), dark: const Color(0xffFFFFFF), listen: listen);
+    return AppTheme.getByTheme(context, light: const Color(0xff082A4D), dark: const Color(0xff082A4D), listen: listen);
   }
 
   static Color borderColor(BuildContext context, {bool listen = true}) {
-    return AppTheme.getByTheme(context, light: const Color(0xffDBDBDB), dark: const Color(0xffDBDBDB), listen: listen);
+    return AppTheme.getByTheme(context, light: const Color(0xffE5E7EB), dark: const Color(0xffE5E7EB), listen: listen);
   }
 
   static Color lightGreyColor(BuildContext context, {bool listen = true}) {
-    return AppTheme.getByTheme(context, light: const Color(0xffE8E8E8), dark: const Color(0xffE8E8E8), listen: listen);
+    return AppTheme.getByTheme(context, light: const Color(0xffEEF0F3), dark: const Color(0xffEEF0F3), listen: listen);
   }
 
   static Color yellowColor(BuildContext context, {bool listen = true}) {
@@ -108,47 +108,38 @@ class AppColor {
   }
 
   static Color redColor(BuildContext context, {bool listen = true}) {
-    return AppTheme.getByTheme(context, light: const Color(0xffFF0000), dark: const Color(0xffFF0000), listen: listen);
+    return AppTheme.getByTheme(context, light: const Color(0xffE5484D), dark: const Color(0xffE5484D), listen: listen);
   }
 
   static Color greenColor(BuildContext context, {bool listen = true}) {
-    return AppTheme.getByTheme(context, light: const Color(0xff14E591), dark: const Color(0xff14E591), listen: listen);
+    return AppTheme.getByTheme(context, light: const Color(0xff16A36A), dark: const Color(0xff16A36A), listen: listen);
   }
 
-  // static Color blueColor(BuildContext context, {bool listen = true}) {
-  //   return AppTheme.getByTheme(
-  //     context,
-  //     light: const Color(0xff5449F8),
-  //     dark: const Color(0xff5449F8),
-  //     listen: listen,
-  //   );
-  // }
-
   static Color inActiveColor(BuildContext context, {bool listen = true}) {
-    return AppTheme.getByTheme(context, light: const Color(0xFFC4C5CE), dark: const Color(0xFFC4C5CE), listen: listen);
+    return AppTheme.getByTheme(context, light: const Color(0xffC7CBD2), dark: const Color(0xffC7CBD2), listen: listen);
   }
 
   static Color searchField(BuildContext context, {bool listen = true}) {
-    return AppTheme.getByTheme(context, light: const Color(0xffF3F6F8), dark: const Color(0xffF3F6F8));
+    return AppTheme.getByTheme(context, light: const Color(0xffF3F5F7), dark: const Color(0xffF3F5F7), listen: listen);
   }
 
   static Color borderColorContainer(BuildContext context, {bool listen = true}) {
-    return AppTheme.getByTheme(context, light: const Color(0xffE4E4E4), dark: const Color(0xffE4E4E4));
+    return AppTheme.getByTheme(context, light: const Color(0xffE6E8EC), dark: const Color(0xffE6E8EC), listen: listen);
   }
 
   static Color gridOneButtonColor(BuildContext context, {bool listen = true}) {
-    return AppTheme.getByTheme(context, light: const Color(0xffFD7201), dark: const Color(0xffFD7201));
+    return AppTheme.getByTheme(context, light: const Color(0xffFD7201), dark: const Color(0xffFD7201), listen: listen);
   }
 
   static Color gridTwoButtonColor(BuildContext context, {bool listen = true}) {
-    return AppTheme.getByTheme(context, light: const Color(0xffFF9D4D), dark: const Color(0xffFF9D4D));
+    return AppTheme.getByTheme(context, light: const Color(0xffFF9D4D), dark: const Color(0xffFF9D4D), listen: listen);
   }
 
   static Color ecececColor(BuildContext context, {bool listen = true}) {
-    return AppTheme.getByTheme(context, light: const Color(0xffECECEC), dark: const Color(0xffECECEC));
+    return AppTheme.getByTheme(context, light: const Color(0xffF1F3F5), dark: const Color(0xffF1F3F5), listen: listen);
   }
 
   static Color ffebbcColor(BuildContext context, {bool listen = true}) {
-    return AppTheme.getByTheme(context, light: const Color(0xffFFEBBC), dark: const Color(0xffFFEBBC));
+    return AppTheme.getByTheme(context, light: const Color(0xffFFF4D8), dark: const Color(0xffFFF4D8), listen: listen);
   }
 }
