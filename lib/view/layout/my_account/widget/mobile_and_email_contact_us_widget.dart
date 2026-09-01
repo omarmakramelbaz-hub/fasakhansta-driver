b8e5e340eb43796d31b848a79e42f42269b571b5
omarmakramelbaz-hub <omarmakramelbaz@gmail.com>
2026-09-01 23:41:05 +0300

    Redesign contact detail tiles

--- lib/view/layout/my_account/widget/mobile_and_email_contact_us_widget.dart
+++ lib/view/layout/my_account/widget/mobile_and_email_contact_us_widget.dart
@@ -1,83 +1,102 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
 
-import '../../../../helpers/images/app_images.dart';
 import '../../../../helpers/locale/app_locale_key.dart';
-import '../../../../helpers/theme/app_colors.dart';
-import '../../../../helpers/theme/app_text_style.dart';
 import '../../../../helpers/utils/url_launcher_methods.dart';
 
 class MobileAndEmailContactUsWidget extends StatelessWidget {
   final String mobile;
   final String email;
+
   const MobileAndEmailContactUsWidget({super.key, required this.mobile, required this.email});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: MediaQuery.of(context).size.width,
-      padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 24),
-      //  height: 169,
-      decoration: const BoxDecoration(
-        image: DecorationImage(fit: BoxFit.cover, image: AssetImage(AppImages.shapeContactUs)),
-      ),
-      child: Column(
-        children: [
-          GestureDetector(
-            onTap: () {
-              UrlLauncherMethods.makePhoneCall(mobile);
-            },
-            child: Row(
-              children: [
-                CircleAvatar(
-                  backgroundColor: AppColor.whiteColor(context),
-                  radius: 25,
-                  child: Center(
-                    child: SvgPicture.asset(
-                      AppImages.callIcon,
-                      colorFilter: ColorFilter.mode(AppColor.blackColor(context), BlendMode.srcIn),
+    return Column(
+      children: [
+        _ContactTile(
+          icon: Icons.phone_in_talk_rounded,
+          label: AppLocaleKey.mobileNumber.tr(),
+          value: mobile,
+          onTap: () => UrlLauncherMethods.makePhoneCall(mobile),
+        ),
+        const SizedBox(height: 10),
+        _ContactTile(
+          icon: Icons.email_outlined,
+          label: AppLocaleKey.email.tr(),
+          value: email,
+          onTap: () => UrlLauncherMethods.makeMailMessage(email),
+        ),
+      ],
+    );
+  }
+}
+
+class _ContactTile extends StatelessWidget {
+  const _ContactTile({
+    required this.icon,
+    required this.label,
+    required this.value,
+    required this.onTap,
+  });
+
+  final IconData icon;
+  final String label;
+  final String value;
+  final VoidCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    const navy = Color(0xff082A4D);
+    const softText = Color(0xff7D8490);
+    const orange = Color(0xffFD7201);
+
+    return Material(
+      color: const Color(0xffFAFAFB),
+      borderRadius: BorderRadius.circular(18),
+      child: InkWell(
+        onTap: onTap,
+        borderRadius: BorderRadius.circular(18),
+        child: Container(
+          padding: const EdgeInsets.all(12),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(18),
+            border: Border.all(color: const Color(0xffECEEF1)),
+          ),
+          child: Row(
+            children: [
+              Container(
+                width: 42,
+                height: 42,
+                decoration: BoxDecoration(
+                  color: const Color(0xffFFF0E3),
+                  borderRadius: BorderRadius.circular(14),
+                ),
+                child: Icon(icon, color: orange, size: 22),
+              ),
+              const SizedBox(width: 11),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      label,
+                      style: const TextStyle(color: softText, fontSize: 11.5, fontWeight: FontWeight.w600),
                     ),
-                  ),
-                ),
-                const SizedBox(width: 14),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(AppLocaleKey.mobileNumber.tr(), style: AppTextStyle.text18BW(context)),
-                    const SizedBox(height: 5),
-                    Text(mobile, style: AppTextStyle.text18BW(context)),
+                    const SizedBox(height: 4),
+                    Text(
+                      value,
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                      style: const TextStyle(color: navy, fontSize: 14, fontWeight: FontWeight.w800),
+                    ),
                   ],
                 ),
-              ],
-            ),
+              ),
+              const Icon(Icons.arrow_outward_rounded, color: orange, size: 19),
+            ],
           ),
-          const SizedBox(height: 30),
-          GestureDetector(
-            onTap: () {
-              UrlLauncherMethods.makeMailMessage(email);
-            },
-            child: Row(
-              children: [
-                CircleAvatar(
-                  backgroundColor: AppColor.whiteColor(context),
-                  radius: 25,
-                  child: Center(child: SvgPicture.asset(AppImages.emailIcon, height: 15)),
-                ),
-                const SizedBox(width: 14),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(AppLocaleKey.email.tr(), style: AppTextStyle.text18BW(context)),
-                    const SizedBox(height: 5),
-                    Text(email, style: AppTextStyle.text18BW(context)),
-                    const SizedBox(height: 5),
-                  ],
-                ),
-              ],
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }
