da9b98d23c9bec96abdcc2627b19ccf2c63e928b
omarmakramelbaz-hub <omarmakramelbaz@gmail.com>
2026-09-01 23:27:17 +0300

    Polish authentication app bar

--- lib/view/custom_widgets/custom_app_bar/custom_auth_app_bar.dart
+++ lib/view/custom_widgets/custom_app_bar/custom_auth_app_bar.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 import '../../../helpers/images/app_images.dart';
 import '../../../helpers/theme/app_colors.dart';
-import '../../../helpers/utils/general_const.dart';
 import '../custom_image/custom_image.dart';
 
 class CustomAuthAppBar extends PreferredSize {
@@ -20,11 +19,12 @@ class CustomAuthAppBar extends PreferredSize {
   final bool automaticallyImplyLeading;
   final BuildContext context;
   final BorderRadiusGeometry? borderRadius;
+
   CustomAuthAppBar(
     this.context, {
     super.key,
-    this.height = 110,
-    this.radius = genRadius,
+    this.height = 92,
+    this.radius = 24,
     this.elevation = 0,
     this.leading,
     this.actions,
@@ -37,65 +37,71 @@ class CustomAuthAppBar extends PreferredSize {
     this.automaticallyImplyLeading = true,
     this.borderRadius,
   }) : super(
-         preferredSize: Size.fromHeight(height),
-         child: AppBar(
-           elevation: elevation,
-           backgroundColor: AppColor.whiteColor(context),
-           toolbarHeight: height,
-           automaticallyImplyLeading: automaticallyImplyLeading,
-           shadowColor: shadowColor,
-           centerTitle: centerTitle,
-           title: title ?? const CustomImage(path: AppImages.appIcon, type: ImageType.asset, height: 60),
-           leading: automaticallyImplyLeading && Navigator.canPop(context) && leading == null
-               ? Center(
-                   child: IconButton(
-                     onPressed: Navigator.canPop(context)
-                         ? () {
-                             Navigator.pop(context);
-                           }
-                         : null,
-                     icon: Icon(Icons.arrow_back_ios_rounded, color: AppColor.whiteColor(context)),
-                   ),
-                 )
-               : leading,
-           actions: actions ?? [const SizedBox(width: 16)],
-           // [
-           //   GestureDetector(
-           //     onTap: () {
-           //       NavigatorMethods.showAppBottomSheet(
-           //           context, const ChangeLangBottomSheet());
-           //     },
-           //     child: const Padding(
-           //       padding: EdgeInsets.symmetric(horizontal: 16),
-           //       child: CustomImage(
-           //         path: AppImages.languageIcon,
-           //         type: ImageType.svg,
-           //         height: 27,
-           //       ),
-           //     ),
-           //   ),
-           // ],
-           leadingWidth: leadingWidth,
-           // flexibleSpace: Container(
-           //   decoration: BoxDecoration(
-           //     borderRadius: borderRadius ??
-           //         BorderRadius.vertical(
-           //           bottom: Radius.circular(radius),
-           //         ),
-           //     gradient: LinearGradient(
-           //       begin: Alignment.topCenter,
-           //       end: Alignment.bottomCenter,
-           //       colors: <Color>[
-           //         AppColor.main2AppColor(context),
-           //         AppColor.main2AppColor(context)
-           //       ],
-           //     ),
-           //   ),
-           // ),
-           shape: RoundedRectangleBorder(
-             borderRadius: borderRadius ?? BorderRadius.vertical(bottom: Radius.circular(radius)),
-           ),
-           bottom: bottom,
-         ),
-       );
+          preferredSize: Size.fromHeight(height),
+          child: AppBar(
+            elevation: elevation,
+            scrolledUnderElevation: 0,
+            backgroundColor: appBarColor ?? AppColor.whiteColor(context),
+            surfaceTintColor: Colors.transparent,
+            toolbarHeight: height,
+            automaticallyImplyLeading: false,
+            shadowColor: shadowColor,
+            centerTitle: centerTitle ?? true,
+            title: title ??
+                const CustomImage(
+                  path: AppImages.appIcon,
+                  type: ImageType.asset,
+                  height: 56,
+                ),
+            leading: leading ??
+                (automaticallyImplyLeading && Navigator.canPop(context)
+                    ? Padding(
+                        padding: const EdgeInsets.all(11),
+                        child: Material(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(14),
+                          child: InkWell(
+                            onTap: () => Navigator.maybePop(context),
+                            borderRadius: BorderRadius.circular(14),
+                            child: Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(14),
+                                border: Border.all(color: const Color(0xffECEEF1)),
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: const Color(0xff082A4D).withOpacity(.07),
+                                    blurRadius: 12,
+                                    offset: const Offset(0, 5),
+                                  ),
+                                ],
+                              ),
+                              child: Icon(
+                                Directionality.of(context) == TextDirection.rtl
+                                    ? Icons.arrow_forward_rounded
+                                    : Icons.arrow_back_rounded,
+                                color: const Color(0xffFD7201),
+                              ),
+                            ),
+                          ),
+                        ),
+                      )
+                    : null),
+            actions: actions ?? const [SizedBox(width: 16)],
+            leadingWidth: leadingWidth ?? 64,
+            flexibleSpace: Container(
+              decoration: BoxDecoration(
+                borderRadius: borderRadius ?? BorderRadius.vertical(bottom: Radius.circular(radius)),
+                gradient: const LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: [Color(0xfffff5ec), Color(0xffffffff)],
+                ),
+              ),
+            ),
+            shape: RoundedRectangleBorder(
+              borderRadius: borderRadius ?? BorderRadius.vertical(bottom: Radius.circular(radius)),
+            ),
+            bottom: bottom,
+          ),
+        );
 }
